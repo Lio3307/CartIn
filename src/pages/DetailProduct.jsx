2058af0f09191489f7f54d4a43fe69cc2460fad8
Lio3307 <aureliofs01@gmail.com>
2025-05-30 13:51:49 +0800

    minor change

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -24,8 +24,6 @@ export function DetailProduct() {
 
     return (
         <>
-
-            
             <div className="container mt-5">
                 <div className="row justify-content-center">
                     <div className="col-lg-10 col-xl-8">
